c4f4cf082909150e7a420d37635e649f6b0f60de
Yojeet <84186427+Circout-sudo@users.noreply.github.com>
2024-12-24 18:22:28 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,1 +1,16 @@
-/
+const allowedPages = [
+    "https://bbis-sports-globe.vercel.app/index.html",
+    "https://bbis-sports-globe.vercel.app/entered.html",
+    "https://bbis-sports-globe.vercel.app/Previous_games.html",
+    "https://bbis-sports-globe.vercel.app/Upcoming_games.html"
+];
+
+// Get the current path from the full URL
+const currentPath = window.location.pathname;
+console.log(currentPath)
+
+// Check if the current page is in the allowed pages list
+if (allowedPages.includes(currentPath)) {
+    alert("This is a simple popup!");
+    window.location.href = "/pass.html"; // Redirect to pass.html
+}
